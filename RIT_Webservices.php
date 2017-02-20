42ae3f20e6252e35f33b1110b4a4a8a782fd7720
Grzegorz Adam Kowalski <grzegorz.adam.kowalski@outlook.com>
2017-02-20 21:39:42 +0100

    Greatly expanded comments (targeting ApiGen)

--- RIT_Webservices.php
+++ RIT_Webservices.php
@@ -1,37 +1,100 @@
 <?php
-/*
-
-  Example usage:
-
-    require 'RIT_Webservices.php';
-
-    $webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
-
-    var_dump($webservice->get_metadata());
-
-		var_dump($webservice->get_objects(array(
-		  'language' => 'ru-RU',
-		  'allForDistributionChannel' => 'true',
-		)));
-
-*/
-
+/**
+ * Class encompassing basic operations on touristic data objects made possible by RIT webservices
+ *
+ * There are three ways to make use of this class, each way utilizing more its methods and requiring less manual work but also requiring
+ * better understanding of how this class works:
+ *
+ * Level 1) Use it only to get properly set-up SoapClient and then proceed with manual request building and response decoding;
+ *
+ * Level 2) Use request creating functions to create proper request objects and call webservices - but no response decoding;
+ *
+ * Level 3) Request creating with some helper functions to decode and process recieved response.
+ *
+ * At level 1 you need to know only about {@see __construct()} and {@see get_webservice}. The latter will give you SoapClient object.
+ *
+ * At level 2 you can start with reading documentation of {@see add_object()} and tracing its helper functions to encode object identifier.
+ * Then you can discover large family of wrappers for {@see get_objects()}. Then there are:
+ * very important {@see get_metadata()},
+ * useful {@see get_events()}
+ * and some more functions are still waiting to be implemented.
+ *
+ * At level 3 you have everything from level 2 but some response decoding functions (at this time primarily focused on metadata) appear:
+ * {@see get_languages()},
+ * {@see get_dictionary()},
+ * {@see get_dictionaries()},
+ * {@see get_dictionary_title},
+ * {@see get_dictionary_values},
+ * {@see get_category()},
+ * {@see get_categories()}.
+ * More are underway.
+ *
+ * If it wasn't enough, you can always subclass RIT_Webservices and access some protected methods. There isn't many of them, but they might be useful:
+ * {@see get_metric()}, {@see get_category_from()}.
+ *
+ * Example:
+ * <code>
+ * require 'RIT_Webservices.php';
+ * $webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
+ * var_dump($webservice->get_metadata());
+ * var_dump($webservice->get_objects(array(
+ *  'language' => 'ru-RU',
+ *  'allForDistributionChannel' => 'true',
+ * )));
+ * </code>
+ *
+ * @author Grzegorz Kowalski
+ *
+ * @todo Complete descriptions of all methods
+ * @todo Complete implementation of RIT webservice API (mass action methods, full language code support, attachments)
+ */
 class RIT_Webservices
 {
+	/**
+	 * User login string
+	 * @var string
+	 */
 	protected $user;
+
+	/**
+	 * RIT instance name, see {@see $instances}
+	 * @var string
+	 */
   protected $instance;
+
+	/**
+	 * Array of options for SoapClient
+	 * @var array
+	 */
   protected $soap_options;
 
+	/**
+	 * Array of available RIT instances
+	 * @var array
+	 */
   public $instances = array(
     'production'  => 'https://intrit.poland.travel/rit/integration/',
     'test'        => 'https://intrittest.poland.travel/rit/integration/',
   );
 
-	/*
-		// Example #1:
-		$webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
-		var_dump($webservice->get_metadata());
-	*/
+/**
+ * Class constructor
+ *
+ * Initializes class with user authentication data and sets target RIT instance.
+ * Lack of certificate file results in exception being thrown.
+ *
+ * Example:
+ * <code>
+ * $webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
+ * var_dump($webservice->get_metadata());
+ * </code>
+ *
+ * @param string $user     Login
+ * @param string $pass     Password
+ * @param string $cert     Path and filename of certificate file (*.pem format)
+ * @param string $instance RIT instance/environment name from {@see $instances}
+ * @throws Exception
+ */
 	public function __construct($user, $pass, $cert, $instance = 'production')
   {
 		$this->user = $user;
@@ -64,20 +127,23 @@ class RIT_Webservices
     );
 	}
 
-	/*
-		Internal function to help create metric object which should be incorporated into SOAP request.
-
-		Example usage:
-
-			$ws	= $this->get_webservice('MetadataOfRIT');
-
-			$request = array(
-				'metric'	=> $this->get_metric(),
-				'language'	=> $lang,
-			);
-
-			return $ws->getMetadataOfRIT($request);
-	*/
+/**
+ * Create new metric object to be used as part of a request object
+ *
+ * Internal function to help create metric object which should be incorporated into SOAP request.
+ *
+ * Example:
+ * <code>
+ * $ws	= $this->get_webservice('MetadataOfRIT');
+ * $request = array(
+ *   'metric'	=> $this->get_metric(),
+ *   'language'	=> $lang,
+ * );
+ * return $ws->getMetadataOfRIT($request);
+ * </code>
+ *
+ * @return object Metric object
+ */
   protected function get_metric()
   {
     return (object) array(
@@ -88,23 +154,27 @@ class RIT_Webservices
     );
   }
 
-	/*
-		Function creating SoapClient object for webservice of given name.
-
-		Example for internal use:
-
-			$ws	= $this->get_webservice('MetadataOfRIT');
-			// ... creating request object ...
-			return $ws->getMetadataOfRIT($request);
-
-		Example for manual SoapClient usage:
-
-			$webservice = new RIT_Webservices('login', 'pass', 'cert.pem', 'env');
-			$client = $webservice->get_webservice('GiveTouristObjects');
-			// ... creating request object
-			$client->addModifyObject($request);
-
-	*/
+/**
+ * Function creating SoapClient object for webservice of given name.
+ *
+ * Example for internal use:
+ * <code>
+ * $ws	= $this->get_webservice('MetadataOfRIT');
+ * // ... creating request object ...
+ * return $ws->getMetadataOfRIT($request);
+ * </code>
+ *
+ * Example for manual SoapClient usage:
+ * <code>
+ * $webservice = new RIT_Webservices('login', 'pass', 'cert.pem', 'env');
+ * $client = $webservice->get_webservice('GiveTouristObjects');
+ * // ... creating request object
+ * $client->addModifyObject($request);
+ * </code>
+ *
+ * @param  string $method_name Name of webservice / method
+ * @return SoapClient Properly set-up and ready to use SoapClient instance
+ */
   public function get_webservice($method_name)
   {
     $url = $this->instances[$this->instance] . $method_name;
@@ -113,17 +183,21 @@ class RIT_Webservices
     return $webservice;
   }
 
-	/*
-		Example (get all objects in Russian language):
-
-		var_dump($webservice->get_objects(array(
-		  'language' => 'ru-RU',
-		  'allForDistributionChannel' => 'true',
-		)));
-
-		// $remote_cache is not implemented now
-	*/
-
+/**
+ * Get all objects satysfying given conditions
+ *
+ * Example:
+ * <code>
+ * var_dump($webservice->get_objects(array(
+ *   'language' => 'ru-RU',
+ *   'allForDistributionChannel' => 'true',
+ * )));
+ * </code>
+ *
+ * @param  array  $where 					Search conditions encoded in array to be inserted into request object
+ * @param  boolean $remote_cache	Set true to get data from cached data; false otherwise
+ * @return object									Response of webservice method call
+ */
 	public function get_objects($where, $remote_cache = false)
   {
     if ($remote_cache === true) {
@@ -140,10 +214,16 @@ class RIT_Webservices
 		return $ws->searchTouristObjects($request);
 	}
 
-	/*
-		Wrapper function for using get_objects() to get all objects in target language.
-	*/
-
+/**
+ * Get all objects in target language
+ *
+ * Wrapper function for using get_objects() to get all objects in target language.
+ *
+ * @param  string  $lang         Language code, see {@see get_languages()}
+ * @param  boolean $remote_cache Set true to get data from cached data; false otherwise
+ * @return object                Response object from webservice
+ * @see    RIT_Webservices::get_languages()
+ */
 	public function get_all_objects($lang = 'pl-PL', $remote_cache = false) {
 		return $this->get_objects(array(
 		  'language' => $lang,
@@ -151,14 +231,26 @@ class RIT_Webservices
 		), $remote_cache);
 	}
 
+	/**
+	 * @ignore
+	 */
 	public function get_objects_by_attributes($attributes) {
 		throw new Exception("Metod not implemented.");
 	}
 
+	/**
+	 * @ignore
+	 */
 	public function get_objects_by_categories($categories) {
 		throw new Exception("Metod not implemented.");
 	}
 
+/**
+ * Recieve single object from RIT database identified by its RIT ID
+ * @param  int|string $rit_id	RIT ID
+ * @param  string $lang   		Language code, see {@see get_languages()}
+ * @return object         		Response object from webservice
+ */
 	public function get_object_by_id($rit_id, $lang = 'pl-PL') {
 		return $this->get_objects(array(
 		  'language' => $lang,
@@ -173,14 +265,14 @@ class RIT_Webservices
 		throw new Exception("Metod not implemented.");
  	}
 
-	/*
-
-		TO DO: description and examples
-
-		See: create_tourist_object()
-
-	*/
-
+/**
+ * Send new object to RIT database
+ *
+ * @param object $object Tourist object encoded using {@see create_tourist_object()}
+ * or manually crafted as 'touristObject' subpart of the addModifyObject request
+ *
+ * @see create_tourist_object()
+ */
 	public function add_object($object)
   {
     $ws	= $this->get_webservice('GiveTouristObjects');
@@ -194,6 +286,16 @@ class RIT_Webservices
 		return $result;
 	}
 
+	/**
+	 * Encode source database ID for touristic data object
+	 *
+	 * @param  string|int $table_id   	Row ID in source database table
+	 * @param  string|null $table_name	(optional) String containing name of source database table if there are multiple tables with touristic objects
+	 * or NULL if there exists only one table with touristic data
+	 * @return object             			Object to be used as ID in {@see create_tourist_object()}
+	 *
+	 * @see create_tourist_object()
+	 */
 	public function encode_object_id($table_id, $table_name = null) {
 		$id = new \stdClass;
 		$id->identifierType = 'I' . ($table_name? 2 : 1);
@@ -204,6 +306,18 @@ class RIT_Webservices
 		return $id;
 	}
 
+	/**
+	 * Encode manually created source unique identifier for touristic data object
+	 *
+	 * In case your touristic objects aren't stored inside database or they can't be identified by simple IDs,
+	 * you can construct unique ID manually by whataver means you choose (concatenation of some fields: e.g. title, address; GUID generation;
+	 * hash calculation).
+	 *
+	 * @param  string $unique_string_id	Unique string ID possibly
+	 * @return object                 	Object to be used as ID in {@see create_tourist_object()}
+	 *
+	 * @see create_tourist_object()
+	 */
 	public function create_object_id($unique_string_id) {
 		$id = new \stdClass;
 		$id->identifierType = 'I3';
@@ -211,16 +325,23 @@ class RIT_Webservices
 		return $id;
 	}
 
-	/*
-		TO DO: description and examples
-
-		object_id -> int or object created by encode_object_id() / create_object_id()
-		last_modified ->
-		categories -> array of strings with category names
-		attributes -> array of key=>values where key contain attribute code and value contains attribute value
-		attachments -> ?
-	*/
-
+/**
+ * Encode tourist data object as subpart of request issued by {@see add_object()}.
+ *
+ * @param  mixed $object_id      RIT ID encoded as int or external ID encoded as object by {@see encode_object_id()} or {@see create_object_id()}
+ * @param  string $last_modified TODO
+ * @param  array  $categories    Array of strings with category names
+ * @param  array  $attributes    Array of key=>values where key contain attribute code and value contains attribute value
+ * @param  array  $attachments	 TODO
+ * @return object                Tourist object to be used in {@see add_object()} call
+ * @see RIT_Webservices::add_object()
+ * @see RIT_Webservices::encode_object_id()
+ * @see RIT_Webservices::create_object_id()
+ *
+ * @todo Add possiblity to add another language versions
+ * @todo Add proper support for attachments
+ * @todo Complete params description
+ */
 	public function create_tourist_object($object_id, $last_modified, $categories, $attributes, $attachments = array()) {
 		$object = new \stdClass;
 		if (is_object($object_id)) {
@@ -254,18 +375,36 @@ class RIT_Webservices
 		return $object;
 	}
 
+	/**
+	 * @ignore
+	 */
 	public function add_objects() {
 		throw new Exception("Metod not implemented.");
 	}
 
+	/**
+	 * @ignore
+	 */
 	public function delete_object() {
 		throw new Exception("Metod not implemented.");
 	}
 
+	/**
+	 * @ignore
+	 */
 	public function delete_objects() {
 		throw new Exception("Metod not implemented.");
 	}
 
+	/**
+	 * @param  string|int $transaction_id	Transaction ID number recieved from mass action methods
+	 * @return object                			Object to be used as ID in {@see create_tourist_object()}
+	 *
+	 * @see add_objects()
+	 * @see delete_objects()
+	 *
+	 * @todo Complete description
+	 */
 	public function get_report($transaction_id) {
 		$ws	= $this->get_webservice('GiveTouristObjects');
 
@@ -277,15 +416,23 @@ class RIT_Webservices
 		return $ws->getReport($request);
 	}
 
-	/*
-		Get all data from getMetadataOfRIT.
-
-		Example:
-			$webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
-			$metadata = $webservice->get_metadata();
-			var_dump($metadata);
-	*/
-
+/**
+ * Recieve all available metadata from metadata webservice
+ *
+ * Notice: metadata webservice does not send complete RIT metadata. Some datasets are ommited due to their large size which considerably
+ * increses response times of the webserve. Those datasets can be found elsewhere in public databases, e.g. list of cities or regions can be found
+ * in public TERYT databases.
+ *
+ * Example:
+ * <code>
+ * $webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
+ * $metadata = $webservice->get_metadata();
+ * var_dump($metadata);
+ * </code>
+ *
+ * @param  string $lang Language code, see {@see get_languages()}
+ * @return object       Response object from webservice call
+ */
 	public function get_metadata($lang = 'pl-PL')
   {
 		$ws	= $this->get_webservice('MetadataOfRIT');
@@ -376,33 +523,55 @@ class RIT_Webservices
 		return null;
 	}
 
-	/*
-
-	Example:
-
-		$webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
-		var_dump($webservice->get_languages());
-
-	Output:
-
-		array(22) {
-			[0]=>
-			string(5) "en-GB"
-			// ...
-			[21]=>
-			string(5) "zh-CN"
-		}
-
-	*/
+/**
+ * Get array of all language codes
+ *
+ * Convenient wrapper for {@see get_dictionary_values()} to get contents of languages dictionary.
+ * This should give the same results regardless of the value of optional *$lang* parameter, so you
+ * can always safely call this method without any arguments.
+ *
+ * Example:
+ * <code>
+ * $webservice = new RIT_Webservices('login', 'password', 'certificate.pem', 'test');
+ * var_dump($webservice->get_languages());
+ * </code>
+ *
+ * Example output:
+ * <pre>
+ * array(22) {
+ *   [0]=> string(5) "en-GB"
+ *   // ...
+ *   [21]=> string(5) "zh-CN"
+ * }
+ * </pre>
+ *
+ * @see RIT_Webservices::get_dictionary_values()
+ *
+ * @param  string $lang (optional) Language code
+ * @return array        Array of all language codes
+ */
 	public function get_languages($lang = 'pl-PL')
 	{
 		return $this->get_dictionary_values('L001', $lang);
 	}
 
+	/**
+	 * @ignore
+	 * [get_file description]
+	 * @param  [type] $file_id [description]
+	 * @return [type]          [description]
+	 */
 	public function get_file($file_id) {
 		throw new Exception("Metod not implemented.");
 	}
 
+/**
+ * @param  string $date_from [description]
+ * @param  string $date_to   [description]
+ * @return object            Response object from webservice call
+ *
+ * @todo Complete description
+ */
 	public function get_events($date_from, $date_to) {
 		$ws	= $this->get_webservice('GetTouristObjectEvents');
 
